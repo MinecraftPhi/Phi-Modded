c8b64465ea256bd85a4d5eb6bbdc0ba9525e2454
vdvman1 <vdvman88@gmail.com>
2019-01-12 15:19:08 +1100

    Fix only every second element being removed from lists when using a predicate (vanilla bug)

--- src/main/java/phi/mixin/PredicateListPathMixin.java
+++ src/main/java/phi/mixin/PredicateListPathMixin.java
@@ -1,8 +1,10 @@
 package phi.mixin;
 
 import java.util.function.BiConsumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,18 +18,38 @@ import phi.CommandStatics;
 @Mixin(targets="net/minecraft/command/arguments/NbtPathArgumentType$class_2207")
 public abstract class PredicateListPathMixin {
     @Shadow
+    @Final
+    private Predicate<Tag> field_9905;
+
+    @Shadow
     private int method_9364(Tag tag_1, BiConsumer<ListTag, Integer> biConsumer_1) {
         return 0;
     }
 
 	@Inject(method = "method_9376", at = @At("HEAD"), cancellable = true)
     private void setAndCheck(Tag tag, Supplier<Tag> tagSupplier, CallbackInfoReturnable<Integer> ci) {
-        System.out.println("setAndCheck");
         ci.setReturnValue(method_9364(tag, (list, i) -> {
             Tag newTag = tagSupplier.get();
             Object originalTag = list.set(i, newTag);
             CommandStatics.CompoundChanged = CommandStatics.CompoundChanged || !newTag.equals(originalTag);
         }));
+        ci.cancel();
+    }
+    
+    @Inject(method = "method_9364", at = @At("HEAD"), cancellable = true)
+    private void updateMatching(Tag tag, BiConsumer<ListTag, Integer> update, CallbackInfoReturnable<Integer> ci) {
+        int count = 0;
+        if (tag instanceof ListTag) {
+            ListTag listTag = (ListTag)tag;
+
+            for(int i = listTag.size(); i >= 0; --i) {
+                if (this.field_9905.test(listTag.get(i))) {
+                    update.accept(listTag, i);
+                    ++count;
+                }
+            }
+        }
+        ci.setReturnValue(count);
         ci.cancel();
 	}
 }
